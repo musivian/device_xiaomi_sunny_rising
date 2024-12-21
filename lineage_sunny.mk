52143c5b1fc1396465df1dc39ce2d6c455b57ebd
musivian <kizura.vian@gmail.com>
2024-12-21 15:31:36 +0800

    sunny: bringup risingos

--- lineage_sunny.mk
+++ lineage_sunny.mk
@@ -12,18 +12,34 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/sunny/device.mk)
 
 # Inherit some common PixelOS stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_DEFAULT_PIXEL_LAUNCHER := true
+TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := false
+TARGET_CORE_GMS := true
+WITH_GMS := true
+
+# List of add-ons
+PRODUCT_PACKAGES += \
+    MarkupGoogle \
+    LatinIMEGooglePrebuilt \
+    AiWallpapers \
+    WallpaperEmojiPrebuilt \
+    PrebuiltDeskClockGoogle \
+    CalculatorGooglePrebuilt \
+    CalendarGooglePrebuilt
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_sunny
+PRODUCT_NAME := lineage_sunny
 PRODUCT_DEVICE := sunny
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := M2101K7AG
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_SYSTEM_NAME := sunny
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    DeviceName=$(PRODUCT_SYSTEM_NAME)
+    DeviceProduct=sunny \
+    SystemName=sunny \
+    RisingMaintainer="ZNAIV" \
+    RisingChipset="Qualcomm SDM678"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
